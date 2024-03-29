1e805cc1bd09ecdaa38529b311926a28ba810c85
D3XT3R <106473446+d3xt33rr@users.noreply.github.com>
2024-03-29 14:21:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -28,11 +28,11 @@ const client = new Client({
 const app = express();
 const port = 3000;
 app.get('/', (req, res) => {
-  res.send('YaY Your Bot Status Changed✨');
+  res.send('Status zostal ustawiony');
 });
 app.listen(port, () => {
-  console.log(`🔗 Listening to RTX: http://localhost:${port}`);
-  console.log(`🔗 Powered By RTX`);
+  console.log(`🔗 Listening to BOOSTMARKET: http://localhost:${port}`);
+  console.log(`🔗 Powered By BOOSTMARKET`);
 });
 
 
@@ -91,9 +91,9 @@ function updateStatusAndSendMessages() {
 }
 
 client.once('ready', () => {
-  console.log(`\x1b[36m%s\x1b[0m`, `|    ✅ Bot is ready as ${client.user.tag}`);
-  console.log(`\x1b[36m%s\x1b[0m`, `|    ✨HAPPY NEW YEAR MY DEAR FAMILY`);
-  console.log(`\x1b[36m%s\x1b[0m`, `|    ❤️WELCOME TO 2024`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ✅ | Bot is ready as ${client.user.tag}`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ✨ | STATUS USTAWIONY`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ❤️ | 2024`);
   updateStatusAndSendMessages();
 
   setInterval(() => {
