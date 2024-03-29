7ab0c7b703de533633555664b4302ea17e3cc6d7
D3XT3R <106473446+d3xt33rr@users.noreply.github.com>
2024-03-29 14:20:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["B00ST MARKET","dsc.gg/boostmarket"];
 
 
 let currentIndex = 0;
